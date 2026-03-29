f50f1184d03d47042d0f0e9ae10f74f53b19b086
JCEEE <0xjceee@proton.me>
2026-03-29 18:55:38 +0100

    add single image loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://cooltrees.net",
+  output: "server",
   integrations: [mdx(), sitemap(), svelte()],
 
   vite: {
