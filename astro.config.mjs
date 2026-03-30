d6547ad750c83bfe97867cda0876c9ff5e07742e
JCEEE <0xjceee@proton.me>
2026-03-30 10:22:01 +0100

    make styatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://cooltrees.net",
-  output: "server",
   integrations: [mdx(), sitemap(), svelte()],
 
   vite: {
