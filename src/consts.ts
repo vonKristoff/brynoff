f3c8fb2510fe2792646fab41bf94cfce9022bc2f
amjam3000 <amirjones@gmail.com>
2026-06-03 15:47:08 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Valley of the Cool Trees";
+export const SITE_TITLE = "Group Camping & Retreat Venue in South Wales | Valley of the Cool Trees";
 export const SITE_DESCRIPTION =
   "Stunning group camping in South Wales for large groups, retreats and gatherings. Exclusive hire site with fire pit, outdoor kitchen, shelter and off‑grid facilities for 20–40 guests.";
 export const S3 = import.meta.env.PROD
